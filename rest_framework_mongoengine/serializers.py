76297f88f75ba0bc2850daab65f404f3317181ab
Dane Freeman <dane.freeman@gtri.gatech.edu>
2014-08-14 08:58:30 -0400

    Initial Hyperlinkserializer

--- rest_framework_mongoengine/serializers.py
+++ rest_framework_mongoengine/serializers.py
@@ -12,6 +12,9 @@ from django.forms import widgets
 from django.utils.datastructures import SortedDict
 from rest_framework.compat import get_concrete_model
 from .fields import ReferenceField, ListField, EmbeddedDocumentField, DynamicField
+from rest_framework.settings import api_settings
+from rest_framework.relations import HyperlinkedRelatedField, HyperlinkedIdentityField
+
 
 
 class MongoEngineModelSerializerOptions(serializers.ModelSerializerOptions):
@@ -245,3 +248,91 @@ class MongoEngineModelSerializer(serializers.ModelSerializer):
                 self._data = self.to_native(obj)
 
         return self._data
+
+
+class HyperlinkedModelSerializerOptions(MongoEngineModelSerializerOptions):
+    """
+    Options for HyperlinkedModelSerializer
+    """
+    def __init__(self, meta):
+        super(HyperlinkedModelSerializerOptions, self).__init__(meta)
+        self.view_name = getattr(meta, 'view_name', None)
+        self.lookup_field = getattr(meta, 'lookup_field', None)
+        self.url_field_name = getattr(meta, 'url_field_name', api_settings.URL_FIELD_NAME)
+
+class HyperlinkedModelSerializer(MongoEngineModelSerializer):
+    """
+    A subclass of ModelSerializer that uses hyperlinked relationships,
+    instead of primary key relationships.
+    """
+    _options_class = HyperlinkedModelSerializerOptions
+    _default_view_name = '%(model_name)s-detail'
+    _hyperlink_field_class = HyperlinkedRelatedField
+    _hyperlink_identify_field_class = HyperlinkedIdentityField
+
+    def get_default_fields(self):
+        fields = super(HyperlinkedModelSerializer, self).get_default_fields()
+
+        if self.opts.view_name is None:
+            self.opts.view_name = self._get_default_view_name(self.opts.model)
+
+        if self.opts.url_field_name not in fields:
+            url_field = self._hyperlink_identify_field_class(
+                view_name=self.opts.view_name,
+                lookup_field=self.opts.lookup_field
+            )
+            ret = self._dict_class()
+            ret[self.opts.url_field_name] = url_field
+            ret.update(fields)
+            fields = ret
+
+        return fields
+
+    def get_pk_field(self, model_field):
+        if self.opts.fields and model_field.name in self.opts.fields:
+            return self.get_field(model_field)
+
+    def get_related_field(self, model_field, related_model, to_many):
+        """
+        Creates a default instance of a flat relational field.
+        """
+        # TODO: filter queryset using:
+        # .using(db).complex_filter(self.rel.limit_choices_to)
+        kwargs = {
+            'queryset': related_model._default_manager,
+            'view_name': self._get_default_view_name(related_model),
+            'many': to_many
+        }
+
+        if model_field:
+            kwargs['required'] = not(model_field.null or model_field.blank)
+            if model_field.help_text is not None:
+                kwargs['help_text'] = model_field.help_text
+            if model_field.verbose_name is not None:
+                kwargs['label'] = model_field.verbose_name
+
+        if self.opts.lookup_field:
+            kwargs['lookup_field'] = self.opts.lookup_field
+
+        return self._hyperlink_field_class(**kwargs)
+
+    def get_identity(self, data):
+        """
+        This hook is required for bulk update.
+        We need to override the default, to use the url as the identity.
+        """
+        try:
+            return data.get(self.opts.url_field_name, None)
+        except AttributeError:
+            return None
+
+    def _get_default_view_name(self, model):
+        """
+        Return the view name to use if 'view_name' is not specified in 'Meta'
+        """
+        model_meta = model._meta
+        format_kwargs = {
+            'app_label': model_meta['collection'],
+            'model_name': model_meta['collection'].lower()
+        }
+        return self._default_view_name % format_kwargs
